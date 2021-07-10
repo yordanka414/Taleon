31b41fad5975ec6d813d36d5277951b5e86f2780
Yordanka414 Yordanka415 <Yordanka414@gmail.com>
2021-07-10 21:35:27 +0300

    Implemented Identity

---
+++ Web/Taleon.Web/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Taleon.Data;
+using Taleon.Data.Models;
+
+[assembly: HostingStartup(typeof(Taleon.Web.Areas.Identity.IdentityHostingStartup))]
+namespace Taleon.Web.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
